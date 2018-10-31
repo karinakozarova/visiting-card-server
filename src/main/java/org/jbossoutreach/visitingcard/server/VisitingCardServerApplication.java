840fc50f482fb9da678648bec7d1b7a807e7cc08
KeenWarrior <anujgargcse@gmail.com>
2018-10-31 18:04:36 +0530

    inject dependencies

---
+++ src/main/java/org/jbossoutreach/visitingcard/server/VisitingCardServerApplication.java
@@ -0,0 +1,12 @@
+package org.jbossoutreach.visitingcard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitingCardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitingCardServerApplication.class, args);
+	}
+}

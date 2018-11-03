ebd5ac09b74a7e240c96a65ffee682ae89d37ac0
pranam <plashkari628@gmail.com>
2018-11-03 18:29:32 +0530

    Django project initiated

--- src/src/settings.py
+++ src/src/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'visiting_card',
     }
 }
 

e09086a280d8859c81fd81b83a5f9a54cc4bdf11
pranam <plashkari628@gmail.com>
2018-11-16 21:06:40 +0530

    code refactor (files renamed)

---
+++ main_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models): pass
+# create required entities here
+
+
+class Card(models): pass
+# create required entities here
+
+
+class DroppedCard(models): pass
+# create required entities here
--- visiting_card/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VisitingCardConfig(AppConfig):
-    name = 'visiting_card'

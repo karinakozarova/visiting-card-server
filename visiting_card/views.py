137b149968f10f3bc0d3b6eec80fa0ac696d27d4
pranam <plashkari628@gmail.com>
2018-11-16 19:51:13 +0530

    Starter Code

---
+++ visiting_card/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# Function to gat all the saved cards
+def get_saved_cards():
+    pass
+
+
+# Function to save visible card in near area
+def save_card():
+    pass
+
+
+# Function to find cards nearby
+def search_cards():
+    pass
+
+
+# Function to Drop card at current location
+def drop_card():
+    pass
